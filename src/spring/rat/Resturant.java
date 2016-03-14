1a0a8dbe847d7cb8996b0fd77e50017ce6399d25
ratneshvarma <ratneshkumarvarma@gmail.com>
2016-03-14 23:15:05 +0530

    use property from setter and set the propery in xml file

--- src/spring/rat/Resturant.java
+++ src/spring/rat/Resturant.java
@@ -5,7 +5,14 @@ package spring.rat;
  */
 
 public class Resturant {
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+
+    String noteContent;
+
     public void gretting(){
-        System.out.println("Welcome to our Resturant");
+        //use setter to diplay message and set propert to xml file for nodeContent
+        System.out.println(noteContent);
     }
 }
