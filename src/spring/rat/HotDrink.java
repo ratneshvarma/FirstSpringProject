4114bf0598cde8c737a3d5eb246e15484a442dfa
ratneshvarma <ratneshkumarvarma@gmail.com>
2016-03-14 23:44:06 +0530

    example of construtor way DI

---
+++ src/spring/rat/HotDrink.java
@@ -0,0 +1,8 @@
+package spring.rat;
+
+/**
+ * Created by ratnesh on 14/3/16.
+ */
+public interface HotDrink {
+    public void prepareHotDrink();
+}
---
+++ src/spring/rat/Tea.java
@@ -0,0 +1,10 @@
+package spring.rat;
+
+/**
+ * Created by ratnesh on 14/3/16.
+ */
+public class Tea implements HotDrink {
+    public void prepareHotDrink(){
+        System.out.println("We Are preparing Tea.");
+    }
+}
