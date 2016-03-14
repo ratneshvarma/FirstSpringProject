78185d63d6861aafea5b4c8be24db1fd3b91cf98
ratneshvarma <ratneshkumarvarma@gmail.com>
2016-03-14 22:36:41 +0530

    project added

---
+++ src/spring/rat/FistSpringMain.java
@@ -0,0 +1,15 @@
+package spring.rat;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by ratnesh on 14/3/16.
+ */
+public class FistSpringMain {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
+        Resturant resturant = (Resturant) context.getBean("resturantBean");
+        resturant.gretting();
+    }
+}
---
+++ src/spring/rat/Resturant.java
@@ -0,0 +1,11 @@
+package spring.rat;
+
+/**
+ * Created by ratnesh on 14/3/16.
+ */
+
+public class Resturant {
+    public void gretting(){
+        System.out.println("Welcome to our Resturant");
+    }
+}
