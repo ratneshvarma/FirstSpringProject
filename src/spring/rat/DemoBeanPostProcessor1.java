553c2918f54c40164700917f38c08d1f0d14e891
ratneshvarma <ratneshkumarvarma@gmail.com>
2016-03-18 00:43:22 +0530

    BeanPostProcessor examplewit Ordered InterFare

--- src/spring/rat/DemoBeanPostProcessor1.java
+++ src/spring/rat/DemoBeanPostProcessor1.java
@@ -7,21 +7,21 @@ import org.springframework.core.Ordered;
 /**
  * Created by ratnesh on 18/3/16.
  */
-public class DemoBeanPostProcessor implements BeanPostProcessor,Ordered {
+public class DemoBeanPostProcessor1 implements BeanPostProcessor, Ordered {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("Processing just befor been initialization");
+        System.out.println("Processing just befor been initialization1");
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("Processing just after been initialised");
+        System.out.println("Processing just after been initialised1");
         return bean;
     }
 
     @Override
     public int getOrder() {
-        return 0;
+        return 1;
     }
 }
